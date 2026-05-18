af7d667011fe8adc0c9f93bc46251cda16d37c15
Federico Cacace <cacacefederico05@gmail.com>
2026-05-18 20:43:34 -0300

    setup inicial proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrtSurvey.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
